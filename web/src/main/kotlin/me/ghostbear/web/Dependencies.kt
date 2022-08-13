1d7b903d81d83ec48df55f63fd1a80fa7b2da2f1
Andreas <andreas.everos@gmail.com>
2022-08-13 20:57:30 +0200

    Recreate Logging table

--- web/src/main/kotlin/me/ghostbear/web/Dependencies.kt
+++ web/src/main/kotlin/me/ghostbear/web/Dependencies.kt
@@ -7,6 +7,9 @@ import io.ktor.client.HttpClient
 import io.ktor.client.engine.cio.CIO
 import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
 import io.ktor.serialization.kotlinx.json.json
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.withContext
 import kotlinx.serialization.json.Json
 import me.ghostbear.database.Database
 
@@ -22,18 +25,25 @@ val httpClient = HttpClient(CIO) {
 }
 
 val driver by lazy {
-    val driver = JdbcSqliteDriver("jdbc:sqlite:kumaslash.db")
-    val currentVersion = driver.executeQuery(null, "PRAGMA schema_version;", 0, null)
-        .getLong(0)!!.toInt()
-    if (currentVersion == 0) {
-        Database.Schema.create(driver)
-    } else {
-        val newVersion = Database.Schema.version
-        if (newVersion > currentVersion) {
-            Database.Schema.migrate(driver, currentVersion, newVersion)
+    runBlocking {
+        val driver = JdbcSqliteDriver("jdbc:sqlite:kumaslash.db")
+
+        val currentVersion = withContext(Dispatchers.IO) {
+            val cursor = driver.executeQuery(null, "PRAGMA schema_version;", 0, null)
+            cursor.getLong(0)!!.toInt().also { cursor.close() }
         }
+
+        if (currentVersion == 0) {
+            Database.Schema.create(driver)
+        } else {
+            val newVersion = Database.Schema.version
+            if (newVersion > currentVersion) {
+                Database.Schema.migrate(driver, currentVersion, newVersion)
+            }
+        }
+
+        driver
     }
-    driver
 }
 
 val database = Database(
