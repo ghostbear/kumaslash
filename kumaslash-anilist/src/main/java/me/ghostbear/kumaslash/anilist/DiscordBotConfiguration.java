3b2c53abc38a4988a26494f414d233b48e2705ee
Andreas <andreas.everos@gmail.com>
2023-08-27 14:15:40 +0200

    Cleanup

--- kumaslash-anilist/src/main/java/me/ghostbear/kumaslash/anilist/DiscordBotConfiguration.java
+++ kumaslash-anilist/src/main/java/me/ghostbear/kumaslash/anilist/DiscordBotConfiguration.java
@@ -3,25 +3,16 @@ package me.ghostbear.kumaslash.anilist;
 import discord4j.gateway.intent.IntentSet;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.reactive.DispatcherHandler;
 
 import static discord4j.gateway.intent.Intent.MESSAGE_CONTENT;
 
 @Configuration
 public class DiscordBotConfiguration {
 
-	private final DispatcherHandler webHandler;
-
-	public DiscordBotConfiguration(DispatcherHandler webHandler) {
-		this.webHandler = webHandler;
-	}
-
 	@Bean
 	public IntentSet intentSet() {
 		return IntentSet.nonPrivileged()
 				.or(IntentSet.of(MESSAGE_CONTENT));
 	}
-
-
 
 }
