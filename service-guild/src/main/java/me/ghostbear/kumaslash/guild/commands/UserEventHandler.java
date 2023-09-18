a594d7f4cdfe67638b1b8fb1869977c6b443ebd7
Andreas <andreas.everos@gmail.com>
2023-09-18 18:35:59 +0200

    Clean-up UserEventHandler

--- service-guild/src/main/java/me/ghostbear/kumaslash/guild/commands/UserEventHandler.java
+++ service-guild/src/main/java/me/ghostbear/kumaslash/guild/commands/UserEventHandler.java
@@ -3,6 +3,9 @@ package me.ghostbear.kumaslash.guild.commands;
 import discord4j.core.event.domain.interaction.ChatInputInteractionEvent;
 import discord4j.core.object.command.ApplicationCommandInteractionOption;
 import discord4j.core.object.command.ApplicationCommandInteractionOptionValue;
+import discord4j.core.object.entity.Member;
+import discord4j.core.object.entity.Message;
+import discord4j.core.object.entity.User;
 import discord4j.core.spec.EmbedCreateFields;
 import discord4j.core.spec.EmbedCreateSpec;
 import discord4j.discordjson.possible.Possible;
@@ -10,6 +13,8 @@ import me.ghostbear.core.discord4j.annotations.DiscordComponent;
 import me.ghostbear.core.discord4j.annotations.DiscordInteractionHandler;
 import me.ghostbear.core.discord4j.annotations.DiscordInteractionProperties;
 import reactor.core.publisher.Mono;
+
+import java.util.Optional;
 
 @DiscordComponent
 public class UserEventHandler {
@@ -20,53 +25,63 @@ public class UserEventHandler {
 	}
 
 	@DiscordInteractionHandler(name = "user.avatar")
-	public Mono<Void> onSubcommandAvatar(ChatInputInteractionEvent event) {
+	public Mono<?> onSubcommandAvatar(ChatInputInteractionEvent event) {
+		Optional<ApplicationCommandInteractionOption> option = event.getOption("avatar");
+		Boolean forGuild = option
+				.flatMap(o -> o.getOption("for_guild"))
+				.flatMap(ApplicationCommandInteractionOption::getValue)
+				.map(ApplicationCommandInteractionOptionValue::asBoolean)
+				.orElse(true);
+		Mono<User> user = option
+				.flatMap(o -> o.getOption("user"))
+				.flatMap(ApplicationCommandInteractionOption::getValue)
+				.map(ApplicationCommandInteractionOptionValue::asUser)
+				.orElseThrow();
 		return event.deferReply()
-				.then(
-						Mono.defer(() -> Mono.just(
-								event.getOption("avatar")
-										.flatMap(option -> option.getOption("for_guild"))
-										.flatMap(ApplicationCommandInteractionOption::getValue)
-										.map(ApplicationCommandInteractionOptionValue::asBoolean)
-										.orElse(true))))
-				.zipWith(
-						Mono.defer(() -> event.getOption("avatar")
-										.flatMap(option -> option.getOption("user"))
-										.flatMap(ApplicationCommandInteractionOption::getValue)
-										.map(ApplicationCommandInteractionOptionValue::asUser)
-										.orElseThrow())
-								.flatMap(user -> user.asMember(event.getInteraction().getGuildId().orElseThrow())))
-				.flatMap(forGuildAndMember -> event.createFollowup()
-						.withEmbeds(
-								EmbedCreateSpec.builder()
-										.image((forGuildAndMember.getT1() ? forGuildAndMember.getT2().getEffectiveAvatarUrl() : forGuildAndMember.getT2().getAvatarUrl()) + "?size=512")
-										.footer(EmbedCreateFields.Footer.of("Avatar for %s".formatted(forGuildAndMember.getT2().getDisplayName()), null))
-										.build()))
-				.then();
+				.then(Mono.defer(() -> Mono.just(forGuild)))
+				.zipWith(user.flatMap(u -> u.asMember(event.getInteraction().getGuildId().orElseThrow())))
+				.flatMap(tuple -> avatarReply(event, tuple.getT1(), tuple.getT2()));
+	}
+
+	Mono<Message> avatarReply(ChatInputInteractionEvent event, boolean forGuild, Member user) {
+		return event.createFollowup()
+				.withEmbeds(EmbedCreateSpec.builder()
+						.image((forGuild ? user.getEffectiveAvatarUrl() : user.getAvatarUrl()) + "?size=512")
+						.footer(EmbedCreateFields.Footer.of("Avatar for %s".formatted(user.getDisplayName()), null))
+						.build());
 	}
 
 	@DiscordInteractionHandler(name = "user.banner")
 	public Mono<Void> onSubcommandBanner(ChatInputInteractionEvent event) {
-		return Mono.defer(() -> event.getOption("banner")
-						.flatMap(option -> option.getOption("user"))
-						.flatMap(ApplicationCommandInteractionOption::getValue)
-						.map(ApplicationCommandInteractionOptionValue::asUser)
-						.orElseThrow())
-						.flatMap(user -> user.asMember(event.getInteraction().getGuildId().orElseThrow()))
+		Mono<User> user = event.getOption("banner")
+				.flatMap(option -> option.getOption("user"))
+				.flatMap(ApplicationCommandInteractionOption::getValue)
+				.map(ApplicationCommandInteractionOptionValue::asUser)
+				.orElseThrow();
+		return event.deferReply()
+				.then(user)
+				.flatMap(u -> u.asMember(event.getInteraction().getGuildId().orElseThrow()))
 				.filter(member -> member.getBannerUrl().isPresent())
-				.flatMap(member -> event.reply()
-						.withEmbeds(
-								EmbedCreateSpec.builder()
-										.image(
-												member.getBannerUrl()
-														.map(value -> value + "?size=1024")
-														.map(Possible::of)
-														.orElse(Possible.absent()))
-										.footer(EmbedCreateFields.Footer.of("Banner for %s".formatted(member.getDisplayName()), null))
-										.build()))
-				.switchIfEmpty(Mono.defer(() -> event.reply()
-						.withEphemeral(true)
-						.withContent("No banner found for user")))
+				.flatMap(member -> bannerReply(event, member))
+				.switchIfEmpty(Mono.defer(() -> bannerNotFoundReply(event)))
 				.then();
 	}
+
+	Mono<Message> bannerReply(ChatInputInteractionEvent event, Member member) {
+		return event.createFollowup()
+				.withEmbeds(EmbedCreateSpec.builder()
+						.image(member.getBannerUrl()
+								.map(value -> value + "?size=1024")
+								.map(Possible::of)
+								.orElse(Possible.absent()))
+						.footer(EmbedCreateFields.Footer.of("Banner for %s".formatted(member.getDisplayName()), null))
+						.build());
+	}
+
+	Mono<Message> bannerNotFoundReply(ChatInputInteractionEvent event) {
+		return event.createFollowup()
+				.withEphemeral(true)
+				.withContent("No banner found for user");
+	}
+
 }
