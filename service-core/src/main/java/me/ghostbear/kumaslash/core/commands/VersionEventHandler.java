e099bc5eb3ee6aaa3bda8538f88016955d193918
Andreas <andreas.everos@gmail.com>
2023-10-20 21:11:13 +0200

    Annotate nullability in service-core

--- service-core/src/main/java/me/ghostbear/kumaslash/core/commands/VersionEventHandler.java
+++ service-core/src/main/java/me/ghostbear/kumaslash/core/commands/VersionEventHandler.java
@@ -4,6 +4,7 @@ import discord4j.core.event.domain.interaction.ChatInputInteractionEvent;
 import me.ghostbear.core.discord4j.annotations.DiscordComponent;
 import me.ghostbear.core.discord4j.annotations.DiscordInteractionHandler;
 import me.ghostbear.core.discord4j.annotations.DiscordInteractionProperties;
+import org.jetbrains.annotations.NotNull;
 import org.springframework.boot.info.BuildProperties;
 import reactor.core.publisher.Mono;
 
@@ -11,16 +12,19 @@ import java.time.LocalDateTime;
 import java.time.ZoneOffset;
 import java.time.format.DateTimeFormatter;
 import java.time.format.FormatStyle;
+import java.util.Objects;
 
 @DiscordComponent
 public class VersionEventHandler {
 
+	@NotNull
 	public static final DateTimeFormatter DATE_TIME_FORMATTER = DateTimeFormatter.ofLocalizedDateTime(FormatStyle.MEDIUM);
 
+	@NotNull
 	private final BuildProperties buildProperties;
 
-	public VersionEventHandler(BuildProperties buildProperties) {
-		this.buildProperties = buildProperties;
+	public VersionEventHandler(@NotNull BuildProperties buildProperties) {
+		this.buildProperties = Objects.requireNonNull(buildProperties);
 	}
 
 	@DiscordInteractionProperties
@@ -28,13 +32,16 @@ public class VersionEventHandler {
 		return "commands/version.json";
 	}
 
+	@NotNull
 	@DiscordInteractionHandler(name = "version")
-	public Mono<Void> onCommandVersion(ChatInputInteractionEvent event) {
+	public Mono<Void> onCommandVersion(@NotNull ChatInputInteractionEvent event) {
+		Objects.requireNonNull(event);
 		return event.reply()
 				.withEphemeral(true)
 				.withContent(content());
 	}
 
+	@NotNull
 	String content() {
 		return """
 				**Version:** %s
@@ -42,10 +49,12 @@ public class VersionEventHandler {
 				""".formatted(version(), time());
 	}
 
+	@NotNull
 	String version() {
 		return buildProperties.getVersion();
 	}
 
+	@NotNull
 	String time() {
 		LocalDateTime dateTime = LocalDateTime.ofInstant(buildProperties.getTime(), ZoneOffset.UTC);
 		return DATE_TIME_FORMATTER.format(dateTime);
