1fd50176f3f1924801a362de4148d833f8d9c0bc
Andreas <andreas.everos@gmail.com>
2023-12-24 10:38:52 +0100

    Embed user and banner

--- service-guild/src/main/java/kumaslash/user/UserController.java
+++ service-guild/src/main/java/kumaslash/user/UserController.java
@@ -7,12 +7,17 @@
  */
 package kumaslash.user;
 
+import java.awt.*;
 import java.util.Objects;
 import java.util.Optional;
-import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
 import kumaslash.jda.annotations.JDAController;
 import kumaslash.jda.annotations.SlashCommandMapping;
 import kumaslash.jda.events.CommandSupplier;
+import kumaslash.jda.events.ResourceCommandSupplier;
+import net.dv8tion.jda.api.EmbedBuilder;
+import net.dv8tion.jda.api.entities.Member;
+import net.dv8tion.jda.api.entities.Message;
 import net.dv8tion.jda.api.entities.User;
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
 import net.dv8tion.jda.api.interactions.InteractionHook;
@@ -22,6 +27,7 @@ import net.dv8tion.jda.api.interactions.commands.build.Commands;
 import net.dv8tion.jda.api.interactions.commands.build.SubcommandData;
 import net.dv8tion.jda.api.utils.ImageProxy;
 import org.springframework.context.annotation.Bean;
+import org.springframework.core.io.ClassPathResource;
 
 @JDAController
 public class UserController {
@@ -38,60 +44,68 @@ public class UserController {
 						.map(OptionMapping::getAsBoolean)
 						.orElse(false);
 
-		User target = aTarget.getAsUser();
-		ImageProxy avatar = aGuild ? target.getEffectiveAvatar() : target.getAvatar();
-
+		User targetUser = aTarget.getAsUser();
+		Member targetMember = aTarget.getAsMember();
+		ImageProxy avatar;
+		if (aGuild) {
+			avatar = targetMember.getEffectiveAvatar() != null ? targetMember.getEffectiveAvatar() : targetUser.getEffectiveAvatar();
+		} else {
+			avatar = targetUser.getEffectiveAvatar();
+		}
 		InteractionHook interactionHook = event.getHook();
 		if (Objects.isNull(avatar)) {
 			interactionHook
-					.sendMessageFormat("%s doesn't have a avatar", target.getEffectiveName())
+					.sendMessageFormat("%s doesn't have a avatar", targetMember.getEffectiveName())
+					.delay(3, TimeUnit.SECONDS)
+					.flatMap(Message::delete)
 					.queue();
 		} else {
-			interactionHook.sendMessage(avatar.getUrl(1024)).queue();
+			Color accentColor = targetUser.retrieveProfile().map(User.Profile::getAccentColor).complete();
+			interactionHook
+					.sendMessageEmbeds(
+							new EmbedBuilder()
+									.setImage(avatar.getUrl(2048))
+									.setColor(accentColor)
+									.setFooter("Avatar for " + targetMember.getEffectiveName())
+									.build())
+					.queue();
 		}
 	}
 
 	@SlashCommandMapping(name = "user banner")
-	public void getUserBanner(SlashCommandInteractionEvent event)
-			throws ExecutionException, InterruptedException {
+	public void getUserBanner(SlashCommandInteractionEvent event) {
 		event.deferReply().queue();
 
 		OptionMapping aTarget = event.getOption("target");
 		assert aTarget != null;
 
-		User target = aTarget.getAsUser();
-		String bannerUrl = target.retrieveProfile().map(User.Profile::getBannerUrl).submit().get();
+		User targetUser = aTarget.getAsUser();
+		Member targetMember = aTarget.getAsMember();
+		String bannerUrl = targetUser.retrieveProfile().map(User.Profile::getBannerUrl).complete();
 
 		InteractionHook interactionHook = event.getHook();
 		if (Objects.isNull(bannerUrl)) {
 			interactionHook
-					.sendMessageFormat("%s doesn't have a banner", target.getEffectiveName())
+					.sendMessageFormat("%s doesn't have a banner", targetMember.getEffectiveName())
+					.delay(3, TimeUnit.SECONDS)
+					.flatMap(Message::delete)
 					.queue();
 		} else {
+			Color accentColor = targetUser.retrieveProfile().map(User.Profile::getAccentColor).complete();
+			interactionHook
+					.sendMessageEmbeds(
+							new EmbedBuilder()
+									.setImage(bannerUrl)
+									.setColor(accentColor)
+									.setFooter("Banner for " + targetMember.getEffectiveName())
+									.build())
+					.queue();
 			interactionHook.sendMessage(bannerUrl).queue();
 		}
 	}
 
 	@Bean
 	public CommandSupplier userCommandSupplier() {
-		return () ->
-				Commands.slash("user", "User related commands")
-						.addSubcommands(
-								new SubcommandData("avatar", "Get the avatar of a user")
-										.addOption(
-												OptionType.USER,
-												"target",
-												"The user to get the avatar from",
-												true)
-										.addOption(
-												OptionType.BOOLEAN,
-												"guild",
-												"Get the guild avatar if user has one"),
-								new SubcommandData("banner", "Get the banner of a user")
-										.addOption(
-												OptionType.USER,
-												"target",
-												"The user to get the banner from",
-												true));
+		return new ResourceCommandSupplier(new ClassPathResource("user.json"));
 	}
 }
